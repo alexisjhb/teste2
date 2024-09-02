9c60fcbae291fae60f1e8a624efb641d034431e9
Alexis Hernández bolivar <169068450+alexisjhb@users.noreply.github.com>
2024-09-02 19:06:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+
+
+System.out.println("O primeiro número é");
+int número1 = 4;
+
+
+ System.out.println(número1);  
+System.out.println("O segundo número é");
+
+System.out.println("A soma é");
+ int soma = número1 + número2; 
+
+
+ System.out.println(soma);   
+    
+    // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
